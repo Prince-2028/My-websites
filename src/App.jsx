145dc05be74bbce0eca47e56d20683c19d5d7ba3
prince <pk20287022@gmail.com>
2025-04-13 13:51:44 +0530

    add tittle and remove about componnt

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
-import About from "./components/About";
+import Github from "./components/Github";
 import Contact from "./components/Contect";
 import Footer from "./components/Footer";
 
@@ -12,7 +12,7 @@ const App = () => {
       <Header />
       <Hero />
       <Projects />
-      <About />
+      <Github />
       <Contact />
       <Footer />
     </div>
--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const About = () => {
+const Github = () => {
   return (
     <section className="bg-gray-100 py-20 px-6 text-center">
-      <h2 className="text-3xl font-bold mb-6">About Me</h2>
+      <h2 className="text-3xl font-bold mb-6">My-Github</h2>
       <p className="max-w-3xl mx-auto text-gray-700">
         I'm a self-taught developer with a passion for learning and building things. My goal is to create beautiful and performant web applications. I enjoy solving problems and constantly seek opportunities to improve my skills.
       </p>
@@ -11,4 +11,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Github;
