a00c38c8ab785a6bbb948260723173ad88d85ced
prince <pk20287022@gmail.com>
2025-04-19 20:05:53 +0530

    update the email id

--- src/components/Contect.jsx
+++ src/components/Contect.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
       <p className="text-gray-600 mb-4">Feel free to reach out if you'd like to collaborate or just say hello!</p>
       <a href="mailto:princekumar404@outlook.com" className="text-blue-600 text-lg hover:underline">
-        princekumar404@outlook.com
+       pk20287022@gmail.com
       </a>
     </section>
   );
